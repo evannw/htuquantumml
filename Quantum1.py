772cfe78c130d52980ac947722ffdb5126aff36c
Anoop Bhat <noopy.g.bhat@gmail.com>
2019-02-02 11:26:49 -0500

    created a gradient function and some helpers

--- Quantum1.py
+++ Quantum1.py
@@ -12,3 +12,50 @@ class NeuralNet(object):
                 tempL.append(np.random.normal(N,N-1)*np.sqrt(2/(N-1)))
             self.weights.append(tempL)
         self.weights = np.array(self.weights)
+
+    #to iterate through spin states, we just count up to 2**n and
+    #use the binary representation of the count as our state,
+    #replacing 0's with -1's
+    def state_from_iteration(self,i):
+        i_bin = bin(i)[2:]#bin gives you '0b#'
+        i_arr = [int(ch) for ch in i_bin]#now we have an array of 1's and 0's
+        i_arr = list(np.zeros(self.N - len(i_arr))) + i_arr
+        for k, spin in enumerate(i_arr):#convert all 0's to -1's
+            if spin == 0:
+                i_arr[k] = -1
+        return i_arr
+
+    #function to compute current wave function
+    def psi(self):
+        wf = np.zeros(2**self.N)
+        for i in range(2**self.N):#iterate through spin states
+            spin_state = self.state_from_iteration(i)
+            for h in range(2**self.M):#iterate through h states
+                h_state = self.state_from_iteration(h)
+                #next we compute the coefficient for this state
+                wf[i] += np.exp(self.sum_states(spin_state, h_state))
+        return wf      
+    #helper function to compute the sum in the exponential
+    def sum_states(self,spin_state, h_state):
+        summation = 0
+        for i in range(len(spin_state)):
+            for j in range(len(h_state)):
+                summation += self.weights[i,j]*spin_state[i]*h_state[j]
+        return summation
+
+    def grad(self, hamiltonian):
+        del_psi = np.zeros((2**self.N, self.weights.size))
+        for i in range(2**self.N):#iterate through spin states
+            spin_state = self.state_from_iteration(i)
+            for weight_index, weight in np.ndenumerate(self.weights):#take partial with respect to each weight
+                for h in range(2**self.M):#iterate through h states
+                    h_state = self.state_from_iteration(h)
+                    #next we compute the coefficient for this weight and state
+                    del_psi[i,weight_index] += spin_state[weight_index[0]]*h_state[weight_index[1]]*np.exp(self.sum_states(spin_state, h_state))
+        #compute gradient of the energy using product rule
+        left = np.matmul(np.transpose(del_psi),hamiltonian)
+        right = np.matmul(hamiltonian,del_psi)
+        return np.matmul(left,self.psi()) + np.matmul(np.transpose(self.psi()),right)
+    
+    def print_weights(self):
+        print(self.weights)
