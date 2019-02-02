0d14cddd05ee0399efc0247980a59f41b747bd0f
Michael Kronovet <michaelkronovet@Michaels-MacBook-Pro-3.local>
2019-02-01 23:41:53 -0500

    constructor

--- Quantum1.py
+++ Quantum1.py
@@ -1,1 +1,12 @@
+import numpy as np
 
+class NeuralNet(object):
+
+    def __init__(self, N, M):
+        self.weights = []
+        for i in range(N):
+            tempL = []
+            for j in range(M):
+                tempL.append(np.random.normal(N,N-1))
+            self.weights.append(tempL)
+        self.weights = np.array(self.weights)
